936b98f14352546366788dd1261fd3e75fbb9576
javier <javieresos12@gmail.com>
2018-09-06 20:34:27 -0500

    funciones agregadas

---
+++ app/src/main/java/com/example/android/listviewpersonalizado/Datos.java
@@ -0,0 +1,17 @@
+package com.example.android.listviewpersonalizado;
+
+import java.util.ArrayList;
+
+public class Datos {
+    private static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void guardar (Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener (){
+        return personas;
+    }
+
+
+}
